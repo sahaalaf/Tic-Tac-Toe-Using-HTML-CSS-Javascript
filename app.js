f8a4972a23fe544f7fc3db19c4fd50aac66c2491
Sahal Sajeed <sarcasticsahal@gmail.com>
2024-06-20 12:47:23 -0700

    Final

--- app.js
+++ app.js
@@ -7,56 +7,76 @@ let PlayerOTurn = true;
 // win patterns
 const winningPatterns = [
     [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]
-  ];
+];
+
+// game draw
+const gameDraw = () => {
+    let draw = true;
+    for (let box of boxes) {
+        if (box.innerText === "") {
+            draw = false;
+            break;
+        }
+    }
+    if (draw) {
+        msg.innerText = "Game is DRAW!";
+        msgBox.classList.remove("hide");
+        for (let box of boxes) {
+            box.disabled = true;
+        }
+    }
+};
 
 // reset game
-const resetGame = () =>{
+const resetGame = () => {
     PlayerOTurn = true;
-    for(let box of boxes){
+    for (let box of boxes) {
         box.disabled = false;
         box.innerText = "";
-        msgBox.classList.add("hide");
     }
-}
+    msgBox.classList.add("hide");
+    msg.innerText = "";
+};
+
 // show winner
-const showWinner = (winner) =>{
-    msg.innerText = `Congrulations, Winner is Player ${winner}`;
+const showWinner = (winner) => {
+    msg.innerText = `Congratulations, Winner is Player ${winner}`;
     msgBox.classList.remove("hide");
-}
+};
+
 // check winner
-const checkWinner = () =>{
-    for(let pattern of winningPatterns){
+const checkWinner = () => {
+    for (let pattern of winningPatterns) {
         let positionOne = boxes[pattern[0]].innerText;
         let positionTwo = boxes[pattern[1]].innerText;
         let positionThree = boxes[pattern[2]].innerText;
 
-        if(positionOne != "" && positionTwo != "" && positionThree != ""){
-            if(positionOne == positionTwo && positionTwo == positionThree){
-                // disabled button after winner is spotted
-                for(let box of boxes){
-                    box.disabled = true;
-                }
-                showWinner(positionOne);
+        if (positionOne !== "" && positionOne === positionTwo && positionOne === positionThree) {
+            // disable buttons after winner is spotted
+            for (let box of boxes) {
+                box.disabled = true;
             }
+            showWinner(positionOne);
+            return;
         }
     }
-}
-//reset button
-resetBtn.addEventListener
-boxes.forEach((box) =>{
-    box.addEventListener("click", ()=>{
-        console.log("Button is Clicked");
-        if(PlayerOTurn){
+    gameDraw();
+};
+
+// add event listeners to boxes
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        if (PlayerOTurn) {
             box.innerText = "O";
             PlayerOTurn = false;
-        }
-        else{
+        } else {
             box.innerText = "X";
             PlayerOTurn = true;
         }
         box.disabled = true;
         checkWinner();
-    })
-})
+    });
+});
 
+// add event listener to reset button
 resetBtn.addEventListener("click", resetGame);
